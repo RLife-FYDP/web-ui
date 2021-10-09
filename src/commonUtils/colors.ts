12a7cae9a7dd9c0e4d98345d6591c4ffcd92d62d
Justin Li <justin.li@faire.com>
2021-10-09 16:08:57 -0700

    added icons and colors

--- src/commonUtils/colors.ts
+++ src/commonUtils/colors.ts
@@ -1,1 +1,14 @@
-export {}
+const COLORS = {
+  // BLACKS
+  Black: "#000000",
+
+  // BLUES
+  SkyBlue: "#ACD4EF",
+  NavyBlue: "#2B679B",
+  Teal: "#5DA6C1",
+
+  // YELLOWS
+  Yellow: "#F7CA59",
+}
+
+export default COLORS;
