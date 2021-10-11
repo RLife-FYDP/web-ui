3066d9f356161a39ad5673c4a38ad9dd6658c449
Justin Li <justin.li@faire.com>
2021-10-10 20:30:24 -0700

    added top nav bar

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,11 +1,14 @@
 import { observer } from "mobx-react";
 import React from "react";
 
+import { MobileTopNavBar } from "../common/MobileTopNavBar";
+
 @observer
 export class Chat extends React.Component {
   render() {
     return (
       <>
+        <MobileTopNavBar />
         <div>CHAT PAGE</div>
       </>
     );
---
+++ src/components/common/MobileTopNavBar.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import RLifeIcon from '../../icons/RLifeIcon.svg';
+import ProfileIcon from '../../icons/ProfileIcon.svg';
+
+// TODO: Should we make this sticky or scrollable?
+export const MobileTopNavBar: React.FC = () => (
+  <Container>
+    <LogoContainer src={RLifeIcon} />
+    <ProfileContainer src={ProfileIcon} />
+  </Container>
+)
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 16px;
+  height: 48px;
+`;
+
+const LogoContainer = styled.img`
+  height: auto;
+  width: 48px;
+`
+
+const ProfileContainer = styled.img`
+  height: auto;
+  width: auto;
+`;
